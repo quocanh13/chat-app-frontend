6ca569b7504985f6b2c73d87662b3d678c4e75d7
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-05 22:20:22 +0700

    feat: invalid message

--- src/features/auth/auth.dto.ts
+++ src/features/auth/auth.dto.ts
@@ -1,12 +1,26 @@
 import {z} from "zod";
+import { INVALID_SCHEMA_MESSAGE } from "../../shared/constant";
 
 export const RegisterSchema = z.object({
-    username: z.string().min(1).max(30).regex(/^[a-zA-Z0-9]+$/, "Username can only contain letters (a-z, A-Z) and numbers (0-9)"),
-    password: z.string().min(1).max(30),
-    name: z.string().trim().min(1).max(50).regex(/^[\p{L}\s]+$/u, "Name must not contain digit (0-9) and special symbol"),
+    username: z.string()
+        .min(1, INVALID_SCHEMA_MESSAGE.MIN_LENGTH("Username", 1))
+        .max(30, INVALID_SCHEMA_MESSAGE.MAX_LENGTH("Username", 30))
+        .regex(/^[a-zA-Z0-9]+$/, INVALID_SCHEMA_MESSAGE.USERNAME_REGEX),
+    password: z.string()
+        .min(1, INVALID_SCHEMA_MESSAGE.MIN_LENGTH("Password", 1))
+        .max(30, INVALID_SCHEMA_MESSAGE.MAX_LENGTH("Password", 30)),
+    name: z.string().trim()
+        .min(1, INVALID_SCHEMA_MESSAGE.MIN_LENGTH("Name", 1))
+        .max(50, INVALID_SCHEMA_MESSAGE.MAX_LENGTH("Name", 50))
+        .regex(/^[\p{L}\s]+$/u, INVALID_SCHEMA_MESSAGE.NAME_REGEX),
 })
 
 export const LoginSchema = z.object({
-    username: z.string().min(1).max(30).regex(/^[a-zA-Z0-9]+$/, "Username can only contain letters (a-z, A-Z) and numbers (0-9)"),
-    password: z.string().min(1).max(30),
+    username: z.string()
+        .min(1, INVALID_SCHEMA_MESSAGE.MIN_LENGTH("Username", 1))
+        .max(30, INVALID_SCHEMA_MESSAGE.MAX_LENGTH("Username", 30))
+        .regex(/^[a-zA-Z0-9]+$/, INVALID_SCHEMA_MESSAGE.USERNAME_REGEX),
+    password: z.string()
+        .min(1, INVALID_SCHEMA_MESSAGE.MIN_LENGTH("Password", 1))
+        .max(30, INVALID_SCHEMA_MESSAGE.MAX_LENGTH("Password", 30)),
 })
