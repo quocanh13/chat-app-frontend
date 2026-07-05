b8c27b2b79e860fd4a23e6910c67ba2d52ad7102
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-05 23:30:47 +0700

    feat: implement registerMutation

--- src/features/auth/useAuth.ts
+++ src/features/auth/useAuth.ts
@@ -1,19 +1,31 @@
-import { useState } from "react"
-import * as AuthService from "./auth.service"
+import type {z} from "zod";
+import { useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import * as AuthApi from "./auth.api"
+import { ApiError, TOAST_TYPE } from "../../shared/types";
+import type { LoginSchema, RegisterSchema } from "./auth.dto";
+import { useToastStore } from "../../stores/toastStore";
 
-interface RegisterInput{
-    username: string,
-    password: string,
-    name: string
-}
+type RegisterInput = z.infer<typeof RegisterSchema>;
+type LoginInput = z.infer<typeof LoginSchema>;
 
 export function useAuth(){
-    const [isLoading, setIsLoading] = useState(false)
+    const navigate = useNavigate()
+    const { addToast } = useToastStore()
+    const registerMutation = useMutation({
+        mutationFn : AuthApi.register,
+        onSuccess(data){
+            addToast({
+                type : TOAST_TYPE.SUCCESS, 
+                message : "Account created successfully. Redirecting to the login page"
+            })
+            navigate("/login")
+        },
+        onError(error: ApiError){
+            addToast({type : TOAST_TYPE.ERROR, message : error.message})
+        }
+    })
 
-    const register = async (input: RegisterInput) => {
-        setIsLoading(true)
-        const registerResult = await AuthService.register(input)
-        setIsLoading(false)
-            
-    }
+
+    return {registerMutation}
 }
