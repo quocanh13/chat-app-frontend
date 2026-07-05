851b88a84713df1ed09c05a73e659c9c470cfe9e
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-05 22:10:27 +0700

    feat: handle invalid form

---
+++ src/lib/form.ts
@@ -0,0 +1,14 @@
+import type { FieldErrors, FieldValues } from "react-hook-form";
+import { useToastStore } from "../stores/toastStore";
+import { TOAST_TYPE } from "../shared/types";
+
+export function onInvalid<F extends FieldValues>(errors: FieldErrors<F>){
+    console.log(errors)
+    const { addToast } = useToastStore.getState()
+    Object.values(errors).forEach((error)=>{
+        addToast({
+            type : TOAST_TYPE.ERROR,
+            message: error?.message?.toString()
+        })
+    });
+}
