c43145baaa4541449e17aae1acb4104625fafaa9
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-05 23:32:42 +0700

    chore: remove unused import

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import App from './App.tsx'
 
 const queryClient = new QueryClient()
 
