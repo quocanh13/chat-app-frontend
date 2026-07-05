8c22a2aab601676880cdbe01205ac0c25553317a
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-05 22:15:21 +0700

    feat: toast type

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,14 +1,33 @@
-export type ServiceResult<Data = undefined> = {
-    success: boolean,
-    data?: Data,
-    code?: string,
-    message?: string,
-    detail?: string
-}
 
 export type ApiResponse<Data = undefined> = {
     data?: Data,
     code?: string,
     message?: string,
-    detail?: string  
+    detail?: Record<string, string[]> 
+}
+
+export class ApiError extends Error{
+    constructor(message?: string, code?: string, detail?: Record<string, string[]>){
+        super(message? message : "Server Error")
+        this.name = "ApiError"
+        this.code = code? code : "SERVER_ERROR"
+        this.detail = detail
+    }
+
+    detail?: Record<string, string[]>
+    code: string
+}
+
+export const TOAST_TYPE = {
+    SUCCESS: 0,
+    ERROR: 1,
+    NOTIFICATION: 2,
+} as const;
+export type ToastType = typeof TOAST_TYPE[keyof typeof TOAST_TYPE];
+
+export type Toast = {
+    id: number,
+    type?: ToastType,
+    message?: string,
+    closeTime: number | null
 }
