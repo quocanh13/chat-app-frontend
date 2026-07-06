4288e02b2eb7388bd97fae0fccfa5c4c96007d95
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-06 10:17:22 +0700

    feat: implment handleApiError function

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,6 @@
+import { ApiError, TOAST_TYPE } from "../shared/types";
 import { useAuthStore } from "../stores/authStore"
+import { useToastStore } from "../stores/toastStore";
 
 
 export function request(
@@ -11,4 +13,11 @@ export function request(
         headers.set("Authorization", `Bearer ${token}`);
     }
     return fetch(url, {...options, headers})
+}
+
+export function handleApiError(error: ApiError){
+    const {addToast} = useToastStore.getState()
+    if(error.message == "Failed to fetch")
+        error.message = "Failed to connect to server, try again"
+    addToast({type : TOAST_TYPE.ERROR, message : error.message})
 }
