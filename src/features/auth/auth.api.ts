1762a9f12f8431ca89a0b469d1d13c6917143cb1
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-06 10:16:16 +0700

    feat: login api

--- src/features/auth/auth.api.ts
+++ src/features/auth/auth.api.ts
@@ -1,15 +1,22 @@
 import { request } from "../../lib/api";
 import { API_ENDPOINTS } from "../../shared/constant"
-import { ApiError, type ApiResponse } from "../../shared/types";
+import { ApiError } from "../../shared/types";
 
 interface RegisterInput{
     username: string,
     password: string,
     name: string
 }
+interface LoginInput{
+    username: string,
+    password: string,
+}
 
 interface RegisterData{
     id: number
+}
+interface LoginData{
+    token: string
 }
 
 export async function register(input: RegisterInput) : Promise<RegisterData> {
@@ -28,4 +35,23 @@ export async function register(input: RegisterInput) : Promise<RegisterData> {
         throw new ApiError()
 
     return result as RegisterData
+}
+
+export async function login(input: LoginInput) : Promise<LoginData> {
+    const options: RequestInit = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(input)
+    }
+    const response = await request(API_ENDPOINTS.AUTH.LOGIN, options);
+    const result = await response.json()
+    if(!response.ok)
+        throw new ApiError(result.message!, result.error, result.detail)
+    const data = result as LoginData
+    if(!data?.token)
+        throw new ApiError()
+
+    return data
 }
