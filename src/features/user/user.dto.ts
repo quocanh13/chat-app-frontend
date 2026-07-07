e581562d42709f230b59df2f236a57a0eb95b8cd
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-07 21:21:41 +0700

    feat: get user data schema

---
+++ src/features/user/user.dto.ts
@@ -0,0 +1,8 @@
+import {z} from "zod"
+
+export const GetUserDataSchema = z.object({
+    username: z.string(),
+    name: z.string(),
+    id: z.int().positive(),
+    avatarFileId: z.int().positive().nullable()
+})
