66cbacce266aba01495e7e217c8bf12d23a6c929
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-07 22:31:48 +0700

    feat: get group list api

--- src/features/user/user.api.ts
+++ src/features/user/user.api.ts
@@ -2,23 +2,42 @@ import type z from "zod";
 import { request } from "../../lib/api";
 import { API_ENDPOINTS } from "../../shared/constant"
 import { ApiError } from "../../shared/types";
-import { GetUserDataSchema } from "./user.dto";
+import { GetGroupListSchema, GetUserDataSchema } from "./user.dto";
 
 interface GetUserByIdInput{
-    id: number
+    userId: number
+}
+interface GetGroupListInput{
+    userId: number
 }
 
 type GetUserByIdData = z.infer<typeof GetUserDataSchema>
+type GetGroupListData = z.infer<typeof GetGroupListSchema>
 
 export async function getUserById(input: GetUserByIdInput) : Promise<GetUserByIdData> {
     const options: RequestInit = {
         method: "GET"
     }
-    const response = await request(API_ENDPOINTS.AUTH.USER.GET_USER_BY_ID(input.id), options);
+    const response = await request(API_ENDPOINTS.AUTH.USER.GET_USER_BY_ID(input.userId), options);
     const result = await response.json()
     if(!response.ok)
         throw new ApiError(result.message!, result.error, result.detail)
     const dto = GetUserDataSchema.safeParse(result)
+    if(!dto.success)
+        throw new ApiError()
+
+    return dto.data
+}
+
+export async function getGroupList(input: GetGroupListInput) : Promise<GetGroupListData> {
+    const options: RequestInit = {
+        method: "GET"
+    }
+    const response = await request(API_ENDPOINTS.AUTH.USER.GET_GROUP_LIST(input.userId), options);
+    const result = await response.json()
+    if(!response.ok)
+        throw new ApiError(result.message!, result.error, result.detail)
+    const dto = GetGroupListSchema.safeParse(result)
     if(!dto.success)
         throw new ApiError()
 
