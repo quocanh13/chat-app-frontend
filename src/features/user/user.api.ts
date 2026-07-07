02f79d358e76dac813546dc4d8ff8f65034593d0
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-07 21:22:15 +0700

    feat: get user by id api

---
+++ src/features/user/user.api.ts
@@ -0,0 +1,26 @@
+import type z from "zod";
+import { request } from "../../lib/api";
+import { API_ENDPOINTS } from "../../shared/constant"
+import { ApiError } from "../../shared/types";
+import { GetUserDataSchema } from "./user.dto";
+
+interface GetUserByIdInput{
+    id: number
+}
+
+type GetUserByIdData = z.infer<typeof GetUserDataSchema>
+
+export async function getUserById(input: GetUserByIdInput) : Promise<GetUserByIdData> {
+    const options: RequestInit = {
+        method: "GET"
+    }
+    const response = await request(API_ENDPOINTS.AUTH.USER.GET_USER_BY_ID(input.id), options);
+    const result = await response.json()
+    if(!response.ok)
+        throw new ApiError(result.message!, result.error, result.detail)
+    const dto = GetUserDataSchema.safeParse(result)
+    if(!dto.success)
+        throw new ApiError()
+
+    return dto.data
+}
