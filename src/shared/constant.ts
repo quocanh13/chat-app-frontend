4c39f28adfc42356856b9d117135daa1d241bc44
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-07 21:11:47 +0700

    feat: add get user by id endpoint

--- src/shared/constant.ts
+++ src/shared/constant.ts
@@ -1,10 +1,15 @@
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
 
 export const API_ENDPOINTS = {
-  AUTH: {
-    LOGIN: API_BASE_URL + '/auth/login',
-    REGISTER: API_BASE_URL + '/auth/register',
-  },
+    AUTH: {
+        LOGIN: API_BASE_URL + '/auth/login',
+        REGISTER: API_BASE_URL + '/auth/register',
+        USER: {
+            GET_USER_BY_ID(id: number){
+                return API_BASE_URL + `/users/${id}`
+            }
+        }
+    },
 };
 
 export const INVALID_SCHEMA_MESSAGE = {
