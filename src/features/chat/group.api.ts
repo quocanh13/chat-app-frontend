c38b59761e08eeae8fbb4ae2388f10e8748b0079
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-08 17:22:46 +0700

    feat: get group by id api

--- src/features/chat/group.api.ts
+++ src/features/chat/group.api.ts
@@ -4,6 +4,10 @@ import { API_ENDPOINTS } from "../../shared/constant";
 import { ApiError } from "../../shared/types";
 import { GetMyGroupListSchema } from "./group.dto";
 
+interface getGroupByIdInput{
+    groupId: number
+}
+
 type GetMyGroupListData = z.infer<typeof GetMyGroupListSchema>
 
 export async function getMyGroupList() : Promise<GetMyGroupListData> {
@@ -11,6 +15,21 @@ export async function getMyGroupList() : Promise<GetMyGroupListData> {
         method: "GET"
     }
     const response = await request(API_ENDPOINTS.USER.GET_MY_GROUP_LIST, options);
+    const result = await response.json()
+    if(!response.ok)
+        throw new ApiError(result.message!, result.error, result.detail)
+    const dto = GetMyGroupListSchema.safeParse(result)
+    if(!dto.success)
+        throw new ApiError()
+
+    return dto.data
+}
+
+export async function getGroupById(input: getGroupByIdInput) {
+    const options: RequestInit = {
+        method: "GET"
+    }
+    const response = await request(API_ENDPOINTS.GROUP.GET_GROUP_BY_ID(input.groupId), options);
     const result = await response.json()
     if(!response.ok)
         throw new ApiError(result.message!, result.error, result.detail)
