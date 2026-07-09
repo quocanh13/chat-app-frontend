9ce887dd48408ebd89ae1a9327390efe21b11e4a
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-09 10:24:15 +0700

    refactor: move group schema to group.dto.ts

--- src/features/user/user.dto.ts
+++ src/features/user/user.dto.ts
@@ -7,13 +7,3 @@ export const GetUserDataSchema = z.object({
     avatarFileId: z.int().positive().nullable()
 })
 
-const GroupSchema = z.object({
-    id: z.int().positive(),
-    name: z.string(),
-    lastMessageId: z.int().positive().nullable(),
-    avatarFileId: z.int().positive().nullable()
-});
-
-export const GetMyGroupListSchema = z.object({
-    groups: z.array(GroupSchema)
-})
