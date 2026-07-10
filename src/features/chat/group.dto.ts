fc4aaf4d0596b527a687a07e4f3329824f1a7b71
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-11 00:47:52 +0700

    feat: add MemberSchema

--- src/features/chat/group.dto.ts
+++ src/features/chat/group.dto.ts
@@ -1,12 +1,18 @@
 import {z} from "zod"
 import { MessageSchema } from "./message.dto";
 
+const MemberSchema = z.object({
+    userId: z.int().positive(),
+    role: z.enum(["host", "member"])
+})
+
 export const GroupSchema = z.object({
     id: z.int().positive(),
     name: z.string(),
     lastMessage: MessageSchema.nullable(),
     avatarFileId: z.int().positive().nullable(),
-    hostId: z.int().positive()
+    hostId: z.int().positive(),
+    members: z.array(MemberSchema)
 });
 
 export const GetMyGroupListSchema = z.object({
