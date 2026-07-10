21205db86252cbb9bb0bd59175937f647b7dd360
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-11 00:48:25 +0700

    feat: add searchTerm state

--- src/stores/groupStore.ts
+++ src/stores/groupStore.ts
@@ -3,9 +3,15 @@ import { create } from "zustand";
 interface GroupStore{
     currentGroupId: number | null,
     setCurrentGroupId: (id: number | null) => void,
+
+    searchTerm: string,
+    setSearchTerm : (searchTerm: string) => void
 }
 
 export const useGroupStore = create<GroupStore>((set) => ({
     currentGroupId : null,
     setCurrentGroupId(id) { set({currentGroupId: id}) },
+
+    searchTerm: "",
+    setSearchTerm(searchTerm) { set({searchTerm}) },
 }))
