2b267ad6343d24f92a0b50d51c1241724e2f9847
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-11 17:40:26 +0700

    feat: send file api

---
+++ src/features/file/file.api.ts
@@ -0,0 +1,29 @@
+import type z from "zod";
+import { request } from "../../lib/api";
+import { API_ENDPOINTS } from "../../shared/constant";
+import { ApiError } from "../../shared/types";
+import { PostFileResponseDataSchema } from "./file.dto";
+
+interface SendFileInput{
+    file: File,
+    type: "MESSAGE" | "USER_AVATAR" | "GROUP_AVATAR"
+}
+
+export async function sendFile(input: SendFileInput){
+    const formData = new FormData()
+    formData.append("file", input.file)
+    formData.append("type", input.type)
+    const options: RequestInit = {
+        method: "POST",
+        body : formData
+    }
+    const response = await request(API_ENDPOINTS.FILE.POST_FILE, options);
+    const result = await response.json()
+    if(!response.ok)
+        throw new ApiError(result.message!, result.error, result.detail)
+    const dto = PostFileResponseDataSchema.safeParse(result)
+    if(!dto.success)
+        throw new ApiError()
+
+    return dto.data
+}
