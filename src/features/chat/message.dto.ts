5a7ec09a9403dfaec35a21260a20374b1941bf2b
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-11 17:38:40 +0700

    feat: send message schema

--- src/features/chat/message.dto.ts
+++ src/features/chat/message.dto.ts
@@ -1,6 +1,12 @@
 import {z} from "zod"
 
+export const MessageFormSchema = z.object({
+    content: z.string().max(2000),
+    file: z.file()
+})
+
 export const MessageSchema = z.object({
+    id: z.int().positive(), 
     userId: z.int().positive(),
     groupId: z.int().positive(),
     content: z.string(),
@@ -8,4 +14,13 @@ export const MessageSchema = z.object({
     sentAt: z.coerce.date()
 })
 
+export const SendMessageSchema = z.object({
+    groupId: z.int().positive(),
+    content: z.string(),
+    fileId: z.int().positive().nullable()
+})
+
 export const GetMessageDataSchema = z.array(MessageSchema)
+
+export type Message = z.infer<typeof MessageSchema>
+export type MessageFormData = z.infer<typeof MessageFormSchema>
