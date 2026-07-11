f68984e9fe50611a54c91aeb518f068e58fd06ef
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-11 21:21:14 +0700

    feat: creat group form schema

--- src/features/chat/group.dto.ts
+++ src/features/chat/group.dto.ts
@@ -1,5 +1,6 @@
 import {z} from "zod"
 import { MessageSchema } from "./message.dto";
+import { INVALID_SCHEMA_MESSAGE } from "../../shared/constant";
 
 const MemberSchema = z.object({
     userId: z.int().positive(),
@@ -18,3 +19,17 @@ export const GroupSchema = z.object({
 export const GetMyGroupListSchema = z.object({
     groups: z.array(GroupSchema)
 })
+
+export const CreateGroupFormSchema = z.object({
+    name: z.string()
+            .min(0, INVALID_SCHEMA_MESSAGE.MIN_LENGTH("Group name", 0))
+            .max(50, INVALID_SCHEMA_MESSAGE.MAX_LENGTH("Group name", 50))
+})
+
+export const CreateGroupResponseData = z.object({
+    id: z.int().positive(),
+    name: z.string()
+})
+
+export type GroupData = z.infer<typeof GroupSchema>
+export type GetMyGroupListData = z.infer<typeof GetMyGroupListSchema>
