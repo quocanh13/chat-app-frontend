fc449c0cb37e064dac9611c9f41026b2376ab244
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-12 22:44:48 +0700

    feat: GetFileResponseDataSchema

--- src/features/file/file.dto.ts
+++ src/features/file/file.dto.ts
@@ -3,3 +3,12 @@ import {z} from "zod"
 export const PostFileResponseDataSchema = z.object({
     id: z.int().positive()
 })
+
+export const GetFileResponseDataSchema = z.object({
+    id: z.int().positive(),
+    name: z.string(),
+    mimeType: z.string(),
+    type: z.enum(["MESSAGE", "GROUP_AVATAR", "USER_AVATAR"]),
+    size: z.int().nonnegative(),
+    uploadedAt: z.coerce.date()
+})
