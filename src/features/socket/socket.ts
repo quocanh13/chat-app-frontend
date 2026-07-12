6fbed3ea498bece257cd4d57eada1198ef893a44
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-12 08:14:51 +0700

    feat: socket connection attemp

--- src/features/socket/socket.ts
+++ src/features/socket/socket.ts
@@ -1,9 +1,20 @@
 import { io } from "socket.io-client";
+import { useToastStore } from "../../stores/toastStore";
+
+const MAX_ATTEMP = 10
+const DELAY_TIME = 3000
+let attemp = 0
 
 export const socket = io("http://localhost:3000", {
-    withCredentials: true
+    withCredentials: true,
 });
 
 socket.on("connect_error", (err)=>{
     console.log(err.message);
+    if(attemp >= MAX_ATTEMP)
+        useToastStore.getState().addToast({message: "Failed to establish socket, please reload page to try again"})
+    else{
+        attemp++
+        setTimeout(()=>socket.connect(), DELAY_TIME)
+    }
 });
