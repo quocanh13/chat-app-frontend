20c656ca19abc1f7ef1b7aafbad6dcc95a93b273
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-12 22:46:50 +0700

    feat: updateUser

--- src/features/chat/components/Sidebar.tsx
+++ src/features/chat/components/Sidebar.tsx
@@ -1,10 +1,10 @@
-import React, { useState, type ChangeEvent } from "react";
+import { useRef, useState, type ChangeEvent } from "react";
 import "./Sidebar.css";
 import createRoomIcon from "../../../assets/create-room-icon.png";
 import userInformationIcon from "../../../assets/user-information-icon.png";
 import defaultGroupAvatar from "../../../assets/default-group-avatar.png";
 import defaultUserAvatar from "../../../assets/default-user-avatar.png"; 
-import { useCurrentUser, useUser } from "../../user/useUser";
+import { useCurrentUser, useUser, useUserMutation } from "../../user/useUser";
 import { useGroup, useGroupList, useGroupMutation } from "../useGroup";
 import { useChatStore } from "../../../stores/chatStore";
 import { timeDiff } from "../../../utils/time";
@@ -13,6 +13,9 @@ import type z from "zod";
 import { CreateGroupFormSchema } from "../group.dto";
 import { useForm } from "react-hook-form";
 import { onInvalid } from "../../../lib/form";
+import { API_ENDPOINTS } from "../../../shared/constant";
+import { useToastStore } from "../../../stores/toastStore";
+import { UpdateUserFormSchema, type UpdateUserFormData } from "../../user/user.dto";
 
 type CreateMessageFormData = z.infer<typeof CreateGroupFormSchema>
 
@@ -55,32 +58,105 @@ function SidebarHeader({ onOpenUser, onOpenCreateGroup }: SidebarHeaderProps){
     </div>
 }
 
-// Hộp thoại Thông tin User giữa màn hình
 function UserInfoModal({ onClose }: { onClose: () => void }){
-    const {currentUser} = useCurrentUser()
-    const avatarUrl = currentUser?.avatarFileId ? `/file/${currentUser.avatarFileId}/view` : defaultUserAvatar
+    const { currentUser } = useCurrentUser()
+    const { updateUser } = useUserMutation()
+    const { addToast } = useToastStore()
+    const [selectedAvatarFile, setSelectedAvatarFile] = useState<File | null>(null);
+    const [previewAvatarUrl, setPreviewAvatarUrl] = useState<string | null>(null);
+    
+    const fileInputRef = useRef<HTMLInputElement>(null);
+    const currentAvatarUrl = currentUser?.avatarFileId ? API_ENDPOINTS.FILE.VIEW_FILE(currentUser.avatarFileId) : defaultUserAvatar;
+    const displayAvatarUrl = previewAvatarUrl || currentAvatarUrl;
+
+    const { register, handleSubmit } = useForm<UpdateUserFormData>();
+
+    function handleAvatarClick() {
+        fileInputRef.current?.click();
+    }
+
+    function handleFileChange(e: ChangeEvent<HTMLInputElement>) {
+        const file = e.target.files?.[0];
+        if (file) {
+            setSelectedAvatarFile(file)
+            setPreviewAvatarUrl(URL.createObjectURL(file))
+        }
+    }
+
+    function onValid(data: UpdateUserFormData) {
+        if(!currentUser) 
+            return
+        const info = {
+            name: data.name ? data.name : undefined,
+            email: data.email ? data.email : undefined
+        }
+        const dto = UpdateUserFormSchema.safeParse(info)
+        if(!dto.success){
+            const error = dto.error.flatten().fieldErrors
+            if(error.name) return addToast({message: error.name[0]})
+            if(error.email) return addToast({message: error.email[0]})
+            return
+        }
+        updateUser({...info, avatar: selectedAvatarFile, userId: currentUser?.id})
+    }
 
     return (
         <div className="modal-overlay" onClick={onClose}>
-            {/* stopPropagation để khi click vào khung trắng không bị tự động đóng modal */}
             <div className="modal-container" onClick={(e) => e.stopPropagation()}>
                 <div className="modal-header">
-                    <h5>Thông tin cá nhân</h5>
-                    <button className="modal-close-btn" onClick={onClose}>×</button>
+                    <h5>Chỉnh sửa thông tin</h5>
+                    <button className="modal-close-btn" onClick={onClose} type="button">×</button>
                 </div>
-                <div className="modal-body user-info-content">
-                    <img className="user-info-avatar" src={avatarUrl} alt="User Avatar" />
-                    <h6 className="user-info-name">{currentUser?.name}</h6>
-                    <p className="user-info-username">@{currentUser?.username}</p>
-                </div>
+                
+                <form className="modal-body user-info-content" onSubmit={handleSubmit(onValid)}>
+                    
+                    <div className="avatar-edit-wrapper" onClick={handleAvatarClick} title="Nhấn để đổi ảnh đại diện">
+                        <img className="user-info-avatar editable" src={displayAvatarUrl} alt="User Avatar" />
+                        <div className="avatar-edit-overlay">
+                            <span>Đổi ảnh</span>
+                        </div>
+                    </div>
+                    <input 
+                        type="file" 
+                        accept="image/*"
+                        ref={fileInputRef} 
+                        onChange={handleFileChange}
+                        style={{ display: 'none' }} 
+                    />
+
+                    <p className="user-info-username" style={{marginBottom: "20px"}}>@{currentUser?.username}</p>
+
+                    <div className="form-group edit-user-form-group">
+                        <label>Tên</label>
+                        <input 
+                            type="text" 
+                            className="form-input" 
+                            placeholder={currentUser?.name}
+                            {...register("name")}
+                        />
+                    </div>
+
+                    <div className="form-group edit-user-form-group">
+                        <label>Email</label>
+                        <input 
+                            type="email" 
+                            className="form-input" 
+                            placeholder={currentUser?.email??"Bạn chưa đăng ký email nào"}
+                            {...register("email")}
+                        />
+                    </div>
+
+                    <button type="submit" className="form-submit-btn" style={{marginTop: "24px", width: "100%"}}>
+                        Lưu thay đổi
+                    </button>
+                </form>
             </div>
         </div>
     );
 }
 
-// Hộp thoại Tạo Nhóm giữa màn hình
 function CreateGroupModal({ onClose }: { onClose: () => void }){
-    const { createGroup, isPending } = useGroupMutation()
+    const { createGroup, isCreatePending } = useGroupMutation()
 
     const { register, handleSubmit } = useForm<CreateMessageFormData>({
         resolver: zodResolver(CreateGroupFormSchema)
@@ -112,8 +188,8 @@ function CreateGroupModal({ onClose }: { onClose: () => void }){
                                 {...register("name")}
                             />
                         </div>
-                        <button type="submit" className="form-submit-btn" disabled={isPending}>
-                            {isPending ? "Đang tạo..." : "Tạo nhóm"}
+                        <button type="submit" className="form-submit-btn" disabled={isCreatePending}>
+                            {isCreatePending ? "Đang tạo..." : "Tạo nhóm"}
                         </button>
                     </form>
                 </div>
