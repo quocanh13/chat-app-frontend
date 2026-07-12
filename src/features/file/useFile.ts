260a40c4c5782a6a0305cd5c63b85259129f618f
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-13 02:58:16 +0700

    feat: set stale time

--- src/features/file/useFile.ts
+++ src/features/file/useFile.ts
@@ -1,13 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import * as FileApi from "./file.api"
 
-export function useFile(fileId: number){
+export function useFile(fileId?: number | null){
     const getFileQuery = useQuery({
         queryKey: ["file", fileId],
         queryFn: () => {
-            return FileApi.getFileInformation({fileId})
+            return FileApi.getFileInformation({fileId: fileId!})
         },
-        staleTime: Infinity
+        enabled: !!fileId,
+        staleTime: 5*60*1000
     })
 
     return {file: getFileQuery.data}
