e049114066b0df08677f8974b94b008a391b9f6d
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-12 22:41:05 +0700

    feat: getFileInformation api

--- src/features/file/file.api.ts
+++ src/features/file/file.api.ts
@@ -2,11 +2,14 @@ import type z from "zod";
 import { request } from "../../lib/api";
 import { API_ENDPOINTS } from "../../shared/constant";
 import { ApiError } from "../../shared/types";
-import { PostFileResponseDataSchema } from "./file.dto";
+import { GetFileResponseDataScheme, PostFileResponseDataSchema } from "./file.dto";
 
 interface SendFileInput{
     file: File,
     type: "MESSAGE" | "USER_AVATAR" | "GROUP_AVATAR"
+}
+interface GetFileInformation{
+    fileId: number
 }
 
 export async function sendFile(input: SendFileInput){
@@ -22,6 +25,20 @@ export async function sendFile(input: SendFileInput){
     if(!response.ok)
         throw new ApiError(result.message!, result.error, result.detail)
     const dto = PostFileResponseDataSchema.safeParse(result)
+    if(!dto.success)
+        throw new ApiError()
+
+    return dto.data
+}
+export async function getFileInformation(input: GetFileInformation) {
+    const options: RequestInit = {
+        method: "GET",
+    }
+    const response = await request(API_ENDPOINTS.FILE.GET_FILE(input.fileId), options);
+    const result = await response.json()
+    if(!response.ok)
+        throw new ApiError(result.message!, result.error, result.detail)
+    const dto = GetFileResponseDataScheme.safeParse(result)
     if(!dto.success)
         throw new ApiError()
 
