9abf9ccc074d84f10b47dbb57a9186c3bebf0506
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-12 22:43:16 +0700

    feat: updateUser api

--- src/features/user/user.api.ts
+++ src/features/user/user.api.ts
@@ -1,17 +1,21 @@
-import type z from "zod";
 import { request } from "../../lib/api";
 import { API_ENDPOINTS } from "../../shared/constant"
 import { ApiError } from "../../shared/types";
-import { GetUserDataSchema } from "./user.dto";
+import { GetUserDataSchema, UpdateUserResponseDataSchema } from "./user.dto";
+import { sendFile } from "../file/file.api";
 
 interface GetUserByIdInput{
     userId: number
 }
+interface UpdateUserInput{
+    userId: number,
+    name?: string | null,
+    avatar?: File | null,
+    email?: string | null
+}
 
-type GetUserByIdData = z.infer<typeof GetUserDataSchema>
 
-
-export async function getUserById(input: GetUserByIdInput) : Promise<GetUserByIdData> {
+export async function getUserById(input: GetUserByIdInput) {
     const options: RequestInit = {
         method: "GET"
     }
@@ -20,13 +24,14 @@ export async function getUserById(input: GetUserByIdInput) : Promise<GetUserById
     if(!response.ok)
         throw new ApiError(result.message!, result.error, result.detail)
     const dto = GetUserDataSchema.safeParse(result)
+    console.log(dto.error?.flatten())
     if(!dto.success)
         throw new ApiError()
 
     return dto.data
 }
 
-export async function getMe() : Promise<GetUserByIdData> {
+export async function getMe(){
     const options: RequestInit = {
         method: "GET"
     }
@@ -41,3 +46,31 @@ export async function getMe() : Promise<GetUserByIdData> {
     return dto.data
 }
 
+export async function updateUser(input: UpdateUserInput) {
+    let avatarFileId = null
+    if(input.avatar)
+        avatarFileId = (await sendFile({file: input.avatar, type: "USER_AVATAR"})).id
+
+    const body = {
+        name: input.name,
+        email: input.email,
+        avatarFileId
+    }
+    const options: RequestInit = {
+        method: "PATCH",
+        body: JSON.stringify(body),
+        headers: {
+            "content-type" : "application/json"
+        }
+    }
+    const response = await request(API_ENDPOINTS.USER.PATCH_USER_BY_ID(input.userId), options);
+    const result = await response.json()
+    console.log(result)
+    if(!response.ok)
+        throw new ApiError(result.message!, result.error, result.detail)
+    const dto = UpdateUserResponseDataSchema.safeParse(result)
+    if(!dto.success)
+        throw new ApiError()
+
+    return dto.data
+}
