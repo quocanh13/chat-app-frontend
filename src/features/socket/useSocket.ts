4d3f0d1e531fbaef2416c9ae640f20172177b146
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-12 11:22:34 +0700

    feat: register onNewMember and onNewGroup

--- src/features/socket/useSocket.ts
+++ src/features/socket/useSocket.ts
@@ -1,16 +1,22 @@
 import { useEffect } from "react";
 import { useMessageSocket } from "../chat/message.socket";
 import { socket } from "./socket";
+import { useGroupSocket } from "../chat/group.socket";
 
 export function useSocket(){
     const { onNewMessage } = useMessageSocket()
+    const { onNewMember, onNewGroup } = useGroupSocket()
     useEffect(()=>{
         console.log("Attach socket handler")
         socket.on("message:new", onNewMessage)
+        socket.on("group:new-member", onNewMember)
+        socket.on("group:new", onNewGroup)
 
         return () => {
             console.log("Remove socket handler")
             socket.off("message:new", onNewMessage)
+            socket.off("group:new-member", onNewMember)
+            socket.off("group:new", onNewGroup)
         }
     }, [])
 }
