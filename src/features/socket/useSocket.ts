62027a3eae132f8b321652a74bea9535cbe7f177
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-12 16:28:20 +0700

    feat: register onDeleteMember and onDeleteGroup

--- src/features/socket/useSocket.ts
+++ src/features/socket/useSocket.ts
@@ -5,18 +5,21 @@ import { useGroupSocket } from "../chat/group.socket";
 
 export function useSocket(){
     const { onNewMessage } = useMessageSocket()
-    const { onNewMember, onNewGroup } = useGroupSocket()
+    const { onNewMember, onNewGroup, onDeleteMember, onDeleteGroup } = useGroupSocket()
     useEffect(()=>{
         console.log("Attach socket handler")
         socket.on("message:new", onNewMessage)
         socket.on("group:new-member", onNewMember)
         socket.on("group:new", onNewGroup)
+        socket.on("group:delete-member", onDeleteMember)
+        socket.on("group:delete", onDeleteGroup)
 
         return () => {
             console.log("Remove socket handler")
             socket.off("message:new", onNewMessage)
             socket.off("group:new-member", onNewMember)
             socket.off("group:new", onNewGroup)
+            socket.off("group:delete", onDeleteGroup)
         }
     }, [])
 }
