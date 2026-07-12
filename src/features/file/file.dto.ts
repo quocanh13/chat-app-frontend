d3d958ff7ccc898fd82ccbe9481ff89b6776cca8
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-13 02:57:36 +0700

    chore: remove unused code

--- src/features/file/file.dto.ts
+++ src/features/file/file.dto.ts
@@ -8,7 +8,5 @@ export const GetFileResponseDataSchema = z.object({
     id: z.int().positive(),
     name: z.string(),
     mimeType: z.string(),
-    type: z.enum(["MESSAGE", "GROUP_AVATAR", "USER_AVATAR"]),
-    size: z.int().nonnegative(),
-    uploadedAt: z.coerce.date()
+    size: z.int().nonnegative()
 })
