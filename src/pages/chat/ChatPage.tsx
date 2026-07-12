e7ba09496b307f6064d1a04d06d1989ece5d0355
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-12 07:38:46 +0700

    feat: call useSocket

--- src/pages/chat/ChatPage.tsx
+++ src/pages/chat/ChatPage.tsx
@@ -2,8 +2,11 @@ import { Sidebar } from "../../features/chat/components/Sidebar";
 import { ChatGroup } from "../../features/chat/components/ChatGroup";
 
 import "./ChatPage.css";
+import { useSocket } from "../../features/socket/useSocket";
+import { useAuth } from "../../features/auth/useAuth";
 
 export function ChatPage() {
+    useSocket()
     return <div className="chat-page-container">
         <Sidebar />
         <ChatGroup />
